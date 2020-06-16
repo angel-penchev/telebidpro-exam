631ba7cb1875d73b6854129b7dd3b2cb8a1a7334
Angel <angel.penchev.tsb@gmail.com>
2020-06-16 20:23:05 +0300

    Resolved coords issue
    Resolved a bug where a strawberry on the other side of the map would get infected.

--- strawberries.py
+++ strawberries.py
@@ -5,13 +5,13 @@ def main():
     K, L, R = parse_user_input(3)
     AX, AY = parse_user_input(2)
     try:
-        BX, BY = parse_user_input(2)
-    except:
+        BX, BY = parse_user_input()
+    except UnboundLocalError:
         pass
 
     field = [[1 for j in range(L)] for i in range(K)]
-    field[AX][AY] = 0
-    field[BX][BY] = 0
+    field[AX - 1][AY - 1] = 0
+    field[BX - 1][BY - 1] = 0
     # print_field(field)
 
     directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
@@ -21,21 +21,22 @@ def main():
             for j, cell in enumerate(row):
                 if cell == 0:
                     for direction in directions:
-                        try:
-                            new_field[i - direction[0]][j - direction[1]] = 0
-                        except IndexError:
-                            pass
+                        if not i - direction[0] < 0 and not j - direction[1] < 0:
+                            try:
+                                new_field[i - direction[0]][j - direction[1]] = 0
+                            except IndexError:
+                                pass
         field = new_field
         # print_field(field)
 
     print(sum([row.count(1) for row in field]))
 
 
-def parse_user_input(required_outputs):
+def parse_user_input(required_outputs=None):
     data = input()
     data = data.strip(' \t\n\r')
     data = re.split(r',\s', data)
-    if len(data) != required_outputs:
+    if required_outputs and len(data) != required_outputs:
         raise IOError
     return [int(i) for i in data]
 
